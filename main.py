4f155f8af8359db2d00fdf2fd62d28e8fc549b8c
touahriayacine <medyacinetouahria@gmail.com>
2022-12-12 19:55:13 +0100

    finishing compression functionnality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication
 import sys
 import main_widget_ui as ui
 import chose , results
+import os
 
 class MyApp(QtWidgets.QMainWindow, ui.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -17,22 +18,22 @@ class MyApp(QtWidgets.QMainWindow, ui.Ui_MainWindow):
             self.window = QtWidgets.QMainWindow()
             self.ui = chose.Ui_choseOperation()
             self.ui.setupUi(self.window)
-            self.ui.comp.clicked.connect(self.compression)
-            self.ui.decomp.clicked.connect(self.decompression)
+            self.ui.comp.clicked.connect(lambda: self.do_operation("Compression" , "comp_prog" , "cmp"))
+            self.ui.decomp.clicked.connect(lambda: self.do_operation("Decompression" , "decomp_prog" , "dcmp"))
             self.close()
             self.window.show()
 
-    def compression(self):
-        print(f"compression: {self.path}")
+    def do_operation(self , t , prog_name , extension):
         file_input, file_output , title = self.open_window()
-        title.setText("Compression")
+        title.setText(t)
         self.read_file(self.path , file_input)
-    
-    def decompression(self):
-        print(f"decompression: {self.path}")
-        file_input,file_output , title= self.open_window()
-        title.setText("Decompression")
-        self.read_file(self.path , file_input)
+        tmp = self.path.split("/")
+        path = "/".join(tmp[:-1])
+        name= tmp[-1]
+        new_name = name.split(".")[0] + f"_{extension}.txt"
+        os.system(f"./{prog_name} {self.path}")
+        os.system(f"mv prog_cmp.txt {path}/{new_name}")
+        self.read_file(f"{path}/{new_name}" , file_output)
 
     def open_window(self):
         self.window = QtWidgets.QMainWindow()
